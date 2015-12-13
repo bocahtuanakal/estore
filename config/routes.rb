5ee83343691ef857a65ec29bfe618898181b1ab9
Andry Prakoso <rajakebo@gmail.com>
2015-12-13 11:39:12 +0000

    Add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'static_pages/signup'
 
   resources :products
-  root      'products#index'
+  root      'static_pages#home'
 
 
 
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,20 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+    
+  def setup
+    @base_title = "WazQiTo"
+  
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get signup" do
     get :signup
     assert_response :success
+    assert_select "title", "Sign Up | #{@base_title}"
   end
 
 end
